10b3be347dab2a8a904c8e0812513df49dfed922
Manjit Rabha <78990697+manjitrabhaa@users.noreply.github.com>
2022-12-17 09:54:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# import PyPDF2
+# a = PyPDF2.PdfFileReader("yourfile.pdf")
+# print(a.documentInfo)
+# print(a.getNumPages())
+# print(a.getPage(2).extract_text())
+# str = ""
+# for i in range(1,30):
+#     str += a.getPage(i).extract_text()
+# with open("text.txt", "w", encoding="utf-8") as f:
+#     f.write(str)
+
+
+import PyPDF2
+from PyPDF2 import PdfFileReader
+#Creating a pdf file object
+pdf = open("yourfile","rb")
+#creating pdf reader object
+pdf_reader = PyPDF2.PdfFileReader(pdf)
+#checking number of pages in a pdf file
+print(pdf_reader.numPages)
+#creating a page object
+page = pdf_reader.getPage(5)
+#finally extracting text from the page
+print(page.extractText(2))
+#closing the pdf file
+pdf.close()
